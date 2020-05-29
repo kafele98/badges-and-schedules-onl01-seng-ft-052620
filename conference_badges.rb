c1eff64c181f9379c7c79f77009ac044ad469c82
Zoe Keller <keller.zoe98@gmail.com>
2020-05-29 13:41:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,6 @@
 def badge_maker(name)
-  index=0 
-  formatted= []
-  name.each do |name|
-    formatted<< puts "Hello, my name is #{name}."
-    index+=1
-  end
-  
-  return formatted
+  return "Hello, my name is #{name}"
 end
+
+def badge_maker_creator(array)
+  
