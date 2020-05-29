85817a2175dbfe99e5e3f5c56b2de92291303725
Zoe Keller <keller.zoe98@gmail.com>
2020-05-29 13:47:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,11 @@
 def badge_maker(name)
-  return "Hello, my name is #{name}"
+  return "Hello, my name is #{name}."
 end
 
 def badge_maker_creator(array)
   new_array= []
   array.each do |name|
-    new_array<< ("Hello, my name is #{name}")
+    new_array<< ("Hello, my name is #{name}.")
   end
   return new_array
 end
