f0d8fe83723f984bb34307b103b59b8f5a034aaf
Zoe Keller <keller.zoe98@gmail.com>
2020-05-29 13:55:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,24 +1,4 @@
-def badge_maker(name)
-  return "Hello, my name is #{name}."
-end
 
-def badge_maker_creator(array)
-  new_array= []
-  array.each do |name|
-    new_array.push ("Hello, my name is #{name}.")
-  end
-  return new_array
-end
-
-def assign_rooms(array)
-  roomarray= []
-  counter= 1 
-  array.each do |name|
-    roomarray<< ("Hello, #{name}! You'll be assigned to room #{counter}!")
-    counter += 1 
-  end
-  puts roomarray
-end
   
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
