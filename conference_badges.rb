f902cf5a533e25894fc1176ea553f5a815b039ee
Zoe Keller <keller.zoe98@gmail.com>
2020-05-29 13:54:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,3 +20,12 @@ def assign_rooms(array)
   puts roomarray
 end
   
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  
+  assign_rooms(attendees).each do |badge|
+    puts badge
+  end
+end 
